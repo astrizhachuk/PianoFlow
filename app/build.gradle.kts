98aab235ecfb6a3fe5fef944e70be74d9fb0fced
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-08 18:29:17 +0300

    build: gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 
 android {
     namespace = "com.astrizhachuk.pianoflow"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.astrizhachuk.pianoflow"
