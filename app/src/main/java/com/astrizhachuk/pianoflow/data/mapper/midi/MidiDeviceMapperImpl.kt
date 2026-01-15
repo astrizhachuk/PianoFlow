088ccb75f0cb3030b9759e952891e074ffa59fa3
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-15 13:49:33 +0300

    docs: добавить KDoc для MidiDeviceMapperImpl

--- app/src/main/java/com/astrizhachuk/pianoflow/data/mapper/midi/MidiDeviceMapperImpl.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/data/mapper/midi/MidiDeviceMapperImpl.kt
@@ -6,11 +6,29 @@ import com.astrizhachuk.pianoflow.domain.model.MidiDevice
 import javax.inject.Inject
 
 /**
- * Реализация интерфейса [MidiDeviceMapper].
- * Преобразует [MidiDeviceInfo] из Android MIDI API в доменную модель [MidiDevice].
+ * Реализация интерфейса [MidiDeviceMapper], отвечающая за преобразование данных
+ * из слоя данных (Data) в доменный слой (Domain).
+ *
+ * Основная задача этого класса — преобразовать системную модель [MidiDeviceInfo],
+ * специфичную для Android MIDI API, в чистую доменную модель [MidiDevice],
+ * которая не зависит от каких-либо фреймворков.
  */
 class MidiDeviceMapperImpl @Inject constructor() : MidiDeviceMapper {
 
+    /**
+     * Преобразует объект [MidiDeviceInfo] из Android MIDI API в доменную модель [MidiDevice].
+     *
+     * Извлекает ключевые свойства из `Bundle`, содержащегося в [MidiDeviceInfo]:
+     * - Имя устройства из [MidiDeviceInfo.PROPERTY_NAME].
+     * - Имя производителя из [MidiDeviceInfo.PROPERTY_MANUFACTURER].
+     *
+     * Если какое-либо из этих свойств отсутствует или равно `null`, метод подставляет
+     * пустую строку `""` в качестве значения по умолчанию, обеспечивая стабильность
+     * и предсказуемость доменной модели.
+     *
+     * @param deviceInfo Системный объект с информацией о MIDI-устройстве.
+     * @return Объект доменной модели [MidiDevice], готовый для использования в бизнес-логике.
+     */
     override fun toDomain(deviceInfo: MidiDeviceInfo): MidiDevice {
         val properties = deviceInfo.properties
         val name = properties.getString(MidiDeviceInfo.PROPERTY_NAME)
