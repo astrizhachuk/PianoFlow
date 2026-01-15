ec2fdcf98214d9b6e8fe3d71e53a765f3311032b
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-15 18:12:37 +0300

    test: перейти с Mockito на MockK в MidiRepositoryImplTest

--- app/src/test/java/com/astrizhachuk/pianoflow/data/repository/MidiRepositoryImplTest.kt
+++ app/src/test/java/com/astrizhachuk/pianoflow/data/repository/MidiRepositoryImplTest.kt
@@ -1,25 +1,24 @@
-
 package com.astrizhachuk.pianoflow.data.repository
 
 import com.astrizhachuk.pianoflow.data.datasource.midi.MidiDataSource
 import com.astrizhachuk.pianoflow.domain.model.ConnectionState
+import io.mockk.every
+import io.mockk.mockk
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import org.mockito.kotlin.mock
-import org.mockito.kotlin.whenever
 
 class MidiRepositoryImplTest {
 
-    private val midiDataSource = mock<MidiDataSource>()
+    private val midiDataSource: MidiDataSource = mockk()
 
     @Test
     fun `observeConnectionState should proxy call to data source`() = runTest {
         // Arrange
         val expectedState = ConnectionState.NoDevice
-        whenever(midiDataSource.connectionState).thenReturn(MutableStateFlow(expectedState))
+        every { midiDataSource.connectionState } returns MutableStateFlow(expectedState)
         val repository = MidiRepositoryImpl(midiDataSource)
 
         // Act
