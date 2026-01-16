a5deccd48897fa978fe77be8a0885a71e6f9244b
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-16 22:04:21 +0300

    test: переписать UI-тесты на использование FakeMidiRepository
    Удалены старые интеграционные тесты (`RealUserNotifierIntegrationTest`) и их зависимости (`FakeUserNotifier`, `TestNotificationModule`).
    
    UI-тест `MidiConnectionUITest` переписан для использования `FakeMidiRepository`, что позволяет напрямую симулировать состояния подключения MIDI-устройства (успешное подключение, отключение, ошибка) и проверять соответствующие обновления в UI.

--- app/src/androidTest/java/com/astrizhachuk/pianoflow/ui/MidiConnectionUITest.kt
+++ app/src/androidTest/java/com/astrizhachuk/pianoflow/ui/MidiConnectionUITest.kt
@@ -1,24 +1,28 @@
-
 package com.astrizhachuk.pianoflow.ui
 
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
-import com.astrizhachuk.pianoflow.presentation.model.UserMessage
+import com.astrizhachuk.pianoflow.R
+import com.astrizhachuk.pianoflow.data.di.DataModule
+import com.astrizhachuk.pianoflow.domain.model.ConnectionState
+import com.astrizhachuk.pianoflow.domain.model.MidiDevice
+import com.astrizhachuk.pianoflow.domain.repository.FakeMidiRepository
+import com.astrizhachuk.pianoflow.domain.repository.MidiRepository
 import com.astrizhachuk.pianoflow.presentation.ui.main.MainActivity
-import com.astrizhachuk.pianoflow.presentation.di.NotificationModule
-import com.astrizhachuk.pianoflow.presentation.service.UserNotifier
+import dagger.hilt.android.testing.BindValue
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import javax.inject.Inject
 
-@UninstallModules(NotificationModule::class)
+@UninstallModules(DataModule::class)
 @HiltAndroidTest
 class MidiConnectionUITest {
 
@@ -28,23 +32,54 @@ class MidiConnectionUITest {
     @get:Rule(order = 1)
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
 
-    @Inject
-    lateinit var userNotifier: UserNotifier
+    private val fakeMidiRepository = FakeMidiRepository()
+
+    @BindValue
+    val midiRepository: MidiRepository = fakeMidiRepository
+
+    private lateinit var context: Context
 
     @Before
     fun setup() {
         hiltRule.inject()
+        context = ApplicationProvider.getApplicationContext<Context>()
     }
 
     @Test
-    fun whenMessageIsSent_itIsDisplayedOnScreen() {
-        val testMessage = "This is a test message"
+    fun connectionSuccess_showsSuccessMessage() {
+        // Given
+        val device = MidiDevice(1, "Virtual MIDI", "PianoFlow", "Google")
 
-        activityRule.scenario.onActivity {
-            userNotifier.sendMessage(UserMessage(testMessage))
-        }
+        // When: Simulate a successful connection
+        fakeMidiRepository.emitState(ConnectionState.Connected(device))
 
-        onView(withText(testMessage))
+        // Then: Check that the UI has updated and shows the connected device name
+        onView(withText(context.getString(R.string.midi_device_connected, device.name)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun connectionLost_showsDisconnectedMessage() {
+        // Given: The device was connected
+        val device = MidiDevice(1, "Virtual MIDI", "PianoFlow", "Google")
+        fakeMidiRepository.emitState(ConnectionState.Connected(device))
+
+        // When: Simulate connection loss
+        fakeMidiRepository.emitState(ConnectionState.Disconnected)
+
+        // Then: Check that the UI displays a disconnection message
+        onView(withText(context.getString(R.string.midi_device_disconnected)))
+            .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun connectionError_showsErrorMessage() {
+        // When: Simulate a connection error
+        val errorMessage = context.getString(R.string.midi_error_connection_failed)
+        fakeMidiRepository.emitState(ConnectionState.Error(errorMessage))
+
+        // Then: Check that an error message is displayed on the screen
+        onView(withText(errorMessage))
             .check(matches(isDisplayed()))
     }
 }
