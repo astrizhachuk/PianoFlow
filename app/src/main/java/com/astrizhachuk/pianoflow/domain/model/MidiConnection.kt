58112840a19074aac2bcd3ca4b6a697d9d4d2a96
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-18 18:28:27 +0300

    style: описание класса `MidiConnection`

--- app/src/main/java/com/astrizhachuk/pianoflow/domain/model/MidiConnection.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/domain/model/MidiConnection.kt
@@ -1,12 +1,15 @@
 package com.astrizhachuk.pianoflow.domain.model
 
 /**
- * Domain модель соединения с MIDI-устройством.
+ * Представляет состояние подключения к конкретному MIDI-устройству.
+ *
+ * Этот класс данных моделирует отношение между MIDI-устройством и его текущим
+ * статусом подключения в приложении.
+ *
+ * @property device MIDI-устройство, к которому выполняется подключение.
+ * @property isConnected `true`, если соединение активно, в противном случае `false`.
  */
 data class MidiConnection(
     val device: MidiDevice,
     val isConnected: Boolean
 )
-
-
-
