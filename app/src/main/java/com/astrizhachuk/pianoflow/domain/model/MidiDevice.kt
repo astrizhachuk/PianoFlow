88dbdc25c118e58ae03caf968720cc87059e4d37
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-18 18:26:41 +0300

    style: описание класса `MidiDevice`

--- app/src/main/java/com/astrizhachuk/pianoflow/domain/model/MidiDevice.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/domain/model/MidiDevice.kt
@@ -1,3 +1,14 @@
 package com.astrizhachuk.pianoflow.domain.model
 
+/**
+ * Представляет подключенное MIDI-устройство.
+ *
+ * Этот класс данных содержит информацию, идентифицирующую конкретное устройство ввода или вывода MIDI,
+ * такое как цифровое пианино, клавиатура или синтезатор.
+ *
+ * @property id Уникальный, назначенный системой идентификатор для устройства.
+ * @property name Имя устройства. Для USB-устройств может состоять из имени производителя и продукта.
+ * @property product Название продукта, сообщаемое самим устройством.
+ * @property manufacturer Имя производителя устройства.
+ */
 data class MidiDevice(val id: Int, val name: String, val product: String, val manufacturer: String)
