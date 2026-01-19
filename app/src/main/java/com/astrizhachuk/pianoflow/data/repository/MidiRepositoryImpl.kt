52c2053746e538091bad3ff8c2cf209b95c8c232
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-20 00:27:03 +0300

    docs: добавлены KDoc для интерфейса `MidiRepository` и его реализации.

--- app/src/main/java/com/astrizhachuk/pianoflow/data/repository/MidiRepositoryImpl.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/data/repository/MidiRepositoryImpl.kt
@@ -2,14 +2,29 @@ package com.astrizhachuk.pianoflow.data.repository
 
 import com.astrizhachuk.pianoflow.data.datasource.midi.MidiDataSource
 import com.astrizhachuk.pianoflow.domain.model.ConnectionState
+import com.astrizhachuk.pianoflow.domain.model.Note
 import com.astrizhachuk.pianoflow.domain.repository.MidiRepository
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
+/**
+ * Конкретная реализация интерфейса [MidiRepository].
+ *
+ * Этот класс служит единым источником истины для MIDI-данных, делегируя вызовы
+ * к базовому [MidiDataSource]. Он абстрагирует реализацию источника данных
+ * от доменного слоя, предоставляя чистый API для наблюдения за состоянием MIDI-соединения
+ * и входящими событиями нот.
+ *
+ * @param midiDataSource Источник данных, отвечающий за обработку необработанной MIDI-связи.
+ */
 class MidiRepositoryImpl @Inject constructor(
     private val midiDataSource: MidiDataSource
 ) : MidiRepository {
     override fun observeConnectionState(): Flow<ConnectionState> {
         return midiDataSource.connectionState
+    }
+
+    override fun observeNotes(): Flow<Note> {
+        return midiDataSource.notes
     }
 }
--- app/src/main/java/com/astrizhachuk/pianoflow/domain/repository/MidiRepository.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/domain/repository/MidiRepository.kt
@@ -1,9 +1,32 @@
 package com.astrizhachuk.pianoflow.domain.repository
 
 import com.astrizhachuk.pianoflow.domain.model.ConnectionState
+import com.astrizhachuk.pianoflow.domain.model.Note
 import kotlinx.coroutines.flow.Flow
 
+/**
+ * Репозиторий для управления MIDI-данными.
+ *
+ * Этот интерфейс определяет контракт для взаимодействия с MIDI-устройствами.
+ * Он абстрагирует источник данных и предоставляет чистый API для наблюдения за состоянием
+ * MIDI-соединения и входящими событиями нот.
+ */
 interface MidiRepository {
+    /**
+     * Наблюдает за текущим состоянием подключения MIDI-устройства.
+     *
+     * Эта функция возвращает [Flow], который выдает обновления [ConnectionState] всякий раз,
+     * когда изменяется статус подключения MIDI-устройства (например, подключение, подключено, отключено).
+     *
+     * @return [Flow] из [ConnectionState], представляющий статус MIDI-соединения.
+     */
     fun observeConnectionState(): Flow<ConnectionState>
-}
 
+    /**
+     * Наблюдает за входящими событиями MIDI-нот.
+     *
+     * @return [Flow], который выдает объект [Note] каждый раз, когда от подключенного
+     * MIDI-устройства получается событие включения/выключения ноты.
+     */
+    fun observeNotes(): Flow<Note>
+}
