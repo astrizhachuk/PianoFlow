adb77bf9886e6d60c380f7f4317a1ae08a55e018
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-01-20 15:15:33 +0300

    test: добавить тесты для группировки нот в аккорды

--- app/src/test/java/com/astrizhachuk/pianoflow/domain/usecase/midi/ObserveMidiMessagesUseCaseTest.kt
+++ app/src/test/java/com/astrizhachuk/pianoflow/domain/usecase/midi/ObserveMidiMessagesUseCaseTest.kt
@@ -6,6 +6,7 @@ import com.astrizhachuk.pianoflow.domain.repository.MidiRepository
 import io.mockk.coEvery
 import io.mockk.mockk
 import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.test.runTest
@@ -18,6 +19,7 @@ class ObserveMidiMessagesUseCaseTest {
         const val PITCH_C4 = 60
         const val PITCH_E4 = 64
         const val PITCH_G4 = 67
+        const val CHORD_WINDOW_MS = 50L
     }
 
     private val midiRepository: MidiRepository = mockk()
@@ -70,6 +72,56 @@ class ObserveMidiMessagesUseCaseTest {
             notesChannel.send(singleNote)
             // Проверяем, что нота пришла как новая, отдельная группа
             assertEquals("Single note should be emitted second", listOf(singleNote), awaitItem())
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `invoke with notes inside chord window returns single chord`() = runTest {
+        // Arrange
+        val notesChannel = Channel<Note>(Channel.UNLIMITED)
+        coEvery { midiRepository.observeNotes() } returns notesChannel.receiveAsFlow()
+        val note1 = Note(PITCH_C4)
+        val note2 = Note(PITCH_E4)
+        val note3 = Note(PITCH_G4)
+        val chord = listOf(note1, note2, note3)
+
+        // Act & Assert
+        useCase.invoke().test {
+            notesChannel.send(note1)
+            delay(CHORD_WINDOW_MS - 10)
+            notesChannel.send(note2)
+            delay(CHORD_WINDOW_MS - 10)
+            notesChannel.send(note3)
+
+            assertEquals(chord, awaitItem())
+
+            cancelAndIgnoreRemainingEvents()
+        }
+    }
+
+    @Test
+    fun `invoke with notes outside chord window returns separate notes`() = runTest {
+        // Arrange
+        val notesChannel = Channel<Note>(Channel.UNLIMITED)
+        coEvery { midiRepository.observeNotes() } returns notesChannel.receiveAsFlow()
+        val note1 = Note(PITCH_C4)
+        val note2 = Note(PITCH_E4)
+        val note3 = Note(PITCH_G4)
+
+        // Act & Assert
+        useCase.invoke().test {
+            notesChannel.send(note1)
+            assertEquals(listOf(note1), awaitItem())
+
+            delay(CHORD_WINDOW_MS + 1)
+            notesChannel.send(note2)
+            assertEquals(listOf(note2), awaitItem())
+
+            delay(CHORD_WINDOW_MS + 1)
+            notesChannel.send(note3)
+            assertEquals(listOf(note3), awaitItem())
 
             cancelAndIgnoreRemainingEvents()
         }
