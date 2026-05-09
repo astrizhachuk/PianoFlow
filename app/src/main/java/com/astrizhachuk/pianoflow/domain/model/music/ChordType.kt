ede914094435588f27fd96e57ecc70ab83797bbd
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-05-09 16:27:48 +0300

    feat: add ChordType model and ChordTypeRegistry with 106 chord types

---
+++ app/src/main/java/com/astrizhachuk/pianoflow/domain/model/music/ChordType.kt
@@ -0,0 +1,12 @@
+package com.astrizhachuk.pianoflow.domain.model.music
+
+/**
+ * One chord type from the registry.
+ *
+ * @param chroma 12-bit pitch-class bitmask; bit i is set iff the interval of i semitones from the root is present. Bit 0 is always set (root present).
+ * @param symbol Primary chord symbol appended to the root note name (e.g. "M", "m", "7").
+ */
+internal data class ChordType(
+    val chroma: Int,
+    val symbol: String
+)
