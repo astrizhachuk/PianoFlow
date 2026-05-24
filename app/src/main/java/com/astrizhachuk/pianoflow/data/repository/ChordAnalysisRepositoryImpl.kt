0b1095d601dc150acb7d98f18352ea3330dc02f6
Alexander Strizhachuk <a.strizhachuk@yandex.ru>
2026-05-24 19:44:53 +0300

    docs: обновить комментарии в ChordAnalysisRepositoryImpl для улучшения понимания кода

--- app/src/main/java/com/astrizhachuk/pianoflow/data/repository/ChordAnalysisRepositoryImpl.kt
+++ app/src/main/java/com/astrizhachuk/pianoflow/data/repository/ChordAnalysisRepositoryImpl.kt
@@ -9,6 +9,14 @@ import kotlinx.coroutines.flow.asStateFlow
 import timber.log.Timber
 import javax.inject.Inject
 
+/**
+ * Implementation of [ChordAnalysisRepository] that coordinates chord analysis using a [ChordAnalyzer].
+ *
+ * This repository manages the state of the current chord analysis result and provides
+ * a method to trigger new analyses based on a list of active notes.
+ *
+ * @property chordAnalyzer The service used to identify chords from a set of note names.
+ */
 class ChordAnalysisRepositoryImpl @Inject constructor(
     private val chordAnalyzer: ChordAnalyzer
 ) : ChordAnalysisRepository {
@@ -16,6 +24,15 @@ class ChordAnalysisRepositoryImpl @Inject constructor(
     private val _chordAnalysisResult = MutableStateFlow<String?>(null)
     override val chordAnalysisResult: StateFlow<String?> = _chordAnalysisResult.asStateFlow()
 
+    /**
+     * Analyzes a list of [Note] objects to identify the corresponding chord.
+     *
+     * This function extracts unique note names, sorts them, and uses the [chordAnalyzer]
+     * to determine the chord name. The result is published to [chordAnalysisResult].
+     * If the input list is empty or an error occurs during analysis, the result is set to null.
+     *
+     * @param notes The list of notes to be analyzed.
+     */
     override fun analyzeChord(notes: List<Note>) {
         try {
             Timber.d("analyzeChord: ${notes.size} notes")
